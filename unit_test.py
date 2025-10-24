d15fad298b94fa9d28d216659a98096d6db49642
qkqehenr7 <qkqehenr37@gmail.com>
2025-10-25 06:39:21 +0900

    test: add verification of model support

--- unit_test.py
+++ unit_test.py
@@ -1,3 +1,4 @@
+import pytest
 from run_bench import benchmark
 from utils import gen_model_pte
 import torch
@@ -6,12 +7,16 @@ import os
 # test data
 input_tensor: torch.Tensor = torch.randn(1, 3, 224, 224)
 sample_inputs = (input_tensor,)
-MODEL_NAME = "MobileNetV2"
+
+SUPPORTED_MODELS = ["MobileNetV2", "ResNet18"]
+UNSUPPORTED_MODELS = ["SuperMoblieV7"]
+
 REPEAT = 5
 
-def test_benchmark():
-    model_path = gen_model_pte(model_name=MODEL_NAME, sample_inputs=sample_inputs)
-    assert os.path.exists(model_path), "Not generate pte file."
+@pytest.mark.parametrize("model_name", SUPPORTED_MODELS)
+def test_benchmark_supported(model_name):
+    model_path = gen_model_pte(model_name=model_name, sample_inputs=sample_inputs)
+    assert os.path.exists(model_path), f" pte file not generated for {model_name}."
 
     result = benchmark(model_path, repeat=REPEAT)
 
@@ -21,3 +26,8 @@ def test_benchmark():
     assert "repeat" in result
 
     assert result["repeat"] == REPEAT
+
+@pytest.mark.parametrize("model_name", UNSUPPORTED_MODELS)
+def test_benchmark_unsupported(model_name):
+    with pytest.raises(ValueError):
+        gen_model_pte(model_name=model_name, sample_inputs=sample_inputs)
